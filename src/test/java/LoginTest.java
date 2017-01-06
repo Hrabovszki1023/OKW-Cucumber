bbea4075f83851314a382a18a10b2345b0e82b4f
Hrabovszki1023 <zh@openkeyword.de>
2017-01-06 19:40:38 +0100

    okw dependency:  - se added  - log2html added Testcase: "hallo_okw" created Maven goal: test - works

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,63 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import okw.core.EN;
+import okw.log.Logger_Sngltn;
+import okw.log.log2html.Log2HTML;
+
+public class LoginTest {
+
+	
+    static Log2HTML myLog2html;
+	  /** 
+	   *  \~german
+	   *  Enthält den Namen des aktuellen junit Testfalls.
+	   *  
+	   * @see:   org.junit.rules.TestName
+	   * \~
+	   * @author Zoltan Hrabovszki
+	   * @date 2014.12.03
+	   */
+		@Rule
+		public TestName tcname = new TestName();
+		
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	{
+        Logger_Sngltn myLogger = Logger_Sngltn.getInstance();
+        myLog2html = new Log2HTML();
+
+        // Reset/init the Logger
+        Logger_Sngltn.Init();
+        myLogger.AddLogger(myLog2html);
+
+        myLogger.setDebugMode(false);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		myLog2html.Result2HTML("target/LoginTest.html");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() throws Exception {
+		EN.BeginTest(tcname.getMethodName());
+		
+		
+		EN.EndTest();
+	}
+
+}
